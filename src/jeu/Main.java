fde8e3adfa05c92a109aae766affdcee07b04bec
Priyank Solanki <psolanki@condorcet93.fr>
2022-05-17 09:11:02 +0200

    priyank main

--- src/jeu/Main.java
+++ src/jeu/Main.java
@@ -14,7 +14,6 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = FXMLLoader.load(getClass().getResource("vue/vue1.fxml"));
-			
 			Scene scene = new Scene(root,800,600);
 			root.requestFocus();
 			primaryStage.setScene(scene);
