015dccf702f3a5a5b7df4923a2d47ba83306c527
Emilio <Emilio@10.10.2.116>
2022-06-02 11:49:44 +0200

    commit des classes resources ranger

--- src/jeu/controller/Controller.java
+++ src/jeu/controller/Controller.java
@@ -44,9 +44,6 @@ public class Controller implements Initializable{
 	    private HBox inventaireObjet;
 
 	    @FXML
-	    private HBox inventaireResource;
-
-	    @FXML
 	    private Label labelBois;
 
 	    @FXML
