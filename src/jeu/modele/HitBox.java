33ecfa8c1bb0fba341bdf1a8f4284f8892d28445
xxp90 <xxp90@LAPTOP-I2G4N4J2>
2022-06-04 19:04:41 +0200

    correction bug saut + nuage supprimer + maj du systeme de construction

--- src/jeu/modele/HitBox.java
+++ src/jeu/modele/HitBox.java
@@ -16,11 +16,13 @@ public class HitBox {
 	private int valTabGauche;
 	private TilePane c;
 	private int[] map;
+	private Joueur joueur;
 	
 	//Construceur
 	public HitBox(Joueur joueur, TilePane carte, int[] tabMap) {
 		this.c = carte;
 		this.map = tabMap;
+		this.joueur = joueur;
 		this.ytile = (joueur.getY()+20)/40;
 		this.xtileDroite = (joueur.getX()+40)/40;
 		this.prochaineTuileDroite =  (xtileDroite+(ytile*20));
@@ -32,38 +34,46 @@ public class HitBox {
 	
 	//DROITE
 	public boolean peutPlacerDroite() {
-		return (valTabDroite!=1 && valTabDroite!=2 && valTabDroite!=3 && valTabDroite!=5 && valTabDroite!=6 && valTabDroite!=7 && valTabDroite!=8);
+		if(joueur.getInventaireResource().get(0).getResource().getValue() > 0   )
+			return (valTabDroite!=1 && valTabDroite!=2 && valTabDroite!=3 && valTabDroite!=4 && valTabDroite!=5 && valTabDroite!=6 && valTabDroite!=7);
+		return false;
 	}
 	public boolean peutCasserDroite() {
-		return (valTabDroite!=1 && valTabDroite!=2 && valTabDroite!=3 && valTabDroite!=4 && valTabDroite!=8);
+		return (valTabDroite!=0 && valTabDroite!=1 && valTabDroite!=2 && valTabDroite!=3 && valTabDroite!=7);
 	}
 	public void placerTuileDroite() {
 		c.getChildren().remove(this.prochaineTuileDroite);
-		map[this.prochaineTuileDroite] = 5;
+		map[this.prochaineTuileDroite] = 4;
 		c.getChildren().add(this.prochaineTuileDroite,new ImageView(new Image("jeu/modele/image/map/bois.png")) );
+		joueur.getInventaireResource().get(0).retirerResource();
 	}
 	public void casserTuileDroite() {
 		c.getChildren().remove(this.prochaineTuileDroite);
 		map[this.prochaineTuileDroite] = 0;
 		c.getChildren().add(this.prochaineTuileDroite,new ImageView(new Image("jeu/modele/image/map/ciel.png")) );
+		joueur.getInventaireResource().get(0).ajouterResource();
 	}
 	
 	//GAUCHE
 	public boolean peutPlacerGauche() {
-		return (valTabGauche!=1 && valTabGauche!=2 && valTabGauche!=3 && valTabGauche!=5 && valTabGauche!=6 && valTabGauche!=7 && valTabGauche!=8);
+		if(joueur.getInventaireResource().get(0).getResource().getValue() > 0 )
+			return (valTabGauche!=1 && valTabGauche!=2 && valTabGauche!=3 && valTabGauche!=4 && valTabGauche!=5 && valTabGauche!=6 && valTabGauche!=7);
+		return false;
 	}
 	public boolean peutCasserGauche() {
-		return (valTabGauche!=1 && valTabGauche!=2 && valTabGauche!=3 && valTabGauche!=4 && valTabGauche!=8);
+		return (valTabGauche!=0 && valTabGauche!=1 && valTabGauche!=2 && valTabGauche!=3 && valTabGauche!=7);
 	}
 	public void placerTuileGauche() {
 		c.getChildren().remove(this.prochaineTuileGauche);
-		map[this.prochaineTuileGauche] = 5;
+		map[this.prochaineTuileGauche] = 4;
 		c.getChildren().add(this.prochaineTuileGauche,new ImageView(new Image("jeu/modele/image/map/bois.png")) );
+		joueur.getInventaireResource().get(0).retirerResource();
 	}
 	public void casserTuileGauche() {
 		c.getChildren().remove(this.prochaineTuileGauche);
 		map[this.prochaineTuileGauche] = 0;
 		c.getChildren().add(this.prochaineTuileGauche,new ImageView(new Image("jeu/modele/image/map/ciel.png")) );
+		joueur.getInventaireResource().get(0).ajouterResource();
 	}
 	
 	//Getters
